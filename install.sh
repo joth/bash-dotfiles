9d76a5cc039940e7d90c2a7749bf126172b0e592
Jon Duvald Thuesen <joth@dr.dk>
2016-11-25 21:42:00 +0100

    Add method for creating symbolic links between files

--- install.sh
+++ install.sh
@@ -1,15 +1,23 @@
 #!/usr/bin/env bash
 
 readonly __DIR__="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
-readonly BASHRC_DISPATCH="$__DIR__/bash/modules/bashrc_dispatch/bashrc_dispatch"
+readonly BASH_PROFILE="$__DIR__/bash/modules/bashrc_dispatch/bashrc_dispatch"
 
-if [ ! -f "$BASHRC_DISPATCH" ]; then
-  echo "Missing $BASHRC_DISPATCH. Did you intialize and update submodules?"
+if [ ! -f "$BASH_PROFILE" ]; then
+  echo "Missing $BASH_PROFILE. Did you intialize and update submodules?"
   exit 1
 fi
+
+function create_symbolic_link () {
+  local target="$1"
+  local link_name="$2"
+
+  echo "Creating symbolink link between $target and $link_name"
+  ln -sf "$target" "$link_name"
+}
 
 profiles=( ".bash_profile" ".bashrc" ".bash_login" )
 for profile in "${profiles[@]}"
 do
-  ln -sf "$BASHRC_DISPATCH" "$HOME/$profile"
+  create_symbolic_link "$BASH_PROFILE" "$HOME/$profile"
 done
