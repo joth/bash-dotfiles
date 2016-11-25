e3b78521c4a63821798f93b48165d86ce2cb0cb9
Jon Duvald Thuesen <joth@dr.dk>
2016-11-25 21:42:50 +0100

    Install bashrc_dispatch dotfiles

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 readonly __DIR__="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 readonly BASH_PROFILE="$__DIR__/bash/modules/bashrc_dispatch/bashrc_dispatch"
+readonly BASH_DOTFILES="$__DIR__/bash/dotfiles"
 
 if [ ! -f "$BASH_PROFILE" ]; then
   echo "Missing $BASH_PROFILE. Did you intialize and update submodules?"
@@ -20,4 +21,10 @@ profiles=( ".bash_profile" ".bashrc" ".bash_login" )
 for profile in "${profiles[@]}"
 do
   create_symbolic_link "$BASH_PROFILE" "$HOME/$profile"
+done
+
+dotfiles="$(find "$BASH_DOTFILES" -maxdepth 1 -type f -printf "%f\n")"
+for dotfile in ${dotfiles}
+do
+  create_symbolic_link "$BASH_DOTFILES/$dotfile" "$HOME/.$dotfile"
 done
