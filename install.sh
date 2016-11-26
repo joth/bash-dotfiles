45cc410c60c37c9a59e71170a67e1de9880a4785
Jon Duvald Thuesen <joth@dr.dk>
2016-11-26 10:31:45 +0100

    Install executables into private bin

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 readonly __DIR__="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 readonly BASH_PROFILE="$__DIR__/bash/modules/bashrc_dispatch/bashrc_dispatch"
 readonly BASH_DOTFILES="$__DIR__/bash/dotfiles"
+readonly BASH_PRIVATE_BIN="$__DIR__/bash/bin"
 
 if [ ! -f "$BASH_PROFILE" ]; then
   echo "Missing $BASH_PROFILE. Did you intialize and update submodules?"
@@ -27,4 +28,11 @@ dotfiles="$(find "$BASH_DOTFILES" -maxdepth 1 -type f -printf "%f\n")"
 for dotfile in ${dotfiles}
 do
   create_symbolic_link "$BASH_DOTFILES/$dotfile" "$HOME/.$dotfile"
+done
+
+mkdir -p "$HOME/bin"
+executables="$(find "$BASH_PRIVATE_BIN" -maxdepth 1 -type f -printf "%f\n")"
+for executable in "${executables[@]}"
+do
+  create_symbolic_link "$BASH_PRIVATE_BIN/$executable" "$HOME/bin/$executable"
 done
