0f9dbf5fcddac853d71a8863d38cb5ecc9b754e4
Jon Duvald Thuesen <joth@dr.dk>
2016-11-26 10:49:54 +0100

    Group logic to improve readability

--- install.sh
+++ install.sh
@@ -1,14 +1,34 @@
 #!/usr/bin/env bash
 
+#
+# Shell options
+#
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
+#
+# Script global constants
+#
+
 readonly __DIR__="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 readonly BASH_PROFILE="$__DIR__/bash/modules/bashrc_dispatch/bashrc_dispatch"
 readonly BASH_DOTFILES="$__DIR__/bash/dotfiles"
 readonly BASH_PRIVATE_BIN="$__DIR__/bash/bin"
 
+#
+# Script dependencies
+#
+
 if [ ! -f "$BASH_PROFILE" ]; then
   echo "Missing $BASH_PROFILE. Did you intialize and update submodules?"
   exit 1
 fi
+
+#
+# Script functions
+#
 
 function create_symbolic_link () {
   local target="$1"
@@ -18,21 +38,29 @@ function create_symbolic_link () {
   ln -sf "$target" "$link_name"
 }
 
-profiles=( ".bash_profile" ".bashrc" ".bash_login" )
-for profile in "${profiles[@]}"
-do
-  create_symbolic_link "$BASH_PROFILE" "$HOME/$profile"
-done
+#
+# Script main
+#
 
-dotfiles="$(find "$BASH_DOTFILES" -maxdepth 1 -type f -printf "%f\n")"
-for dotfile in ${dotfiles}
-do
-  create_symbolic_link "$BASH_DOTFILES/$dotfile" "$HOME/.$dotfile"
-done
+function main () {
+  profiles=( ".bash_profile" ".bashrc" ".bash_login" )
+  for profile in "${profiles[@]}"
+  do
+    create_symbolic_link "$BASH_PROFILE" "$HOME/$profile"
+  done
 
-mkdir -p "$HOME/bin"
-executables="$(find "$BASH_PRIVATE_BIN" -maxdepth 1 -type f -printf "%f\n")"
-for executable in "${executables[@]}"
-do
-  create_symbolic_link "$BASH_PRIVATE_BIN/$executable" "$HOME/bin/$executable"
-done
+  dotfiles="$(find "$BASH_DOTFILES" -maxdepth 1 -type f -printf "%f\n")"
+  for dotfile in ${dotfiles}
+  do
+    create_symbolic_link "$BASH_DOTFILES/$dotfile" "$HOME/.$dotfile"
+  done
+
+  mkdir -p "$HOME/bin"
+  executables="$(find "$BASH_PRIVATE_BIN" -maxdepth 1 -type f -printf "%f\n")"
+  for executable in "${executables[@]}"
+  do
+    create_symbolic_link "$BASH_PRIVATE_BIN/$executable" "$HOME/bin/$executable"
+  done
+}
+
+main
